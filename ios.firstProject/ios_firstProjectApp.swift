3f4cf4287af67749a7d2d0533dd0d44d7cd0bcb5
boipqiod <66378324+boipqiod@users.noreply.github.com>
2022-04-07 15:26:51 +0900

    1. 프로젝트 작성

---
+++ ios.firstProject/ios_firstProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_firstProjectApp.swift
+//  ios.firstProject
+//
+//  Created by simon on 2022/04/07.
+//
+
+import SwiftUI
+
+@main
+struct ios_firstProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
