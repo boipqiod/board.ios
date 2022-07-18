bf576f35f9d3885c37cebd7b5ade60deb83b259b
공상헌 <>
2022-07-18 23:48:22 +0900

    ADD 1. BoardDetial, newPost 초기 작업

---
+++ board.ios/Views/Board/BoardDetailViewController.swift
@@ -0,0 +1,14 @@
+//
+//  BoardDetailViewController.swift
+//  board.ios
+//
+//  Created by 공상헌 on 2022/07/18.
+//
+
+import Foundation
+
+class BoardDetailViewController: BaseViewController{
+    
+    
+    
+}
